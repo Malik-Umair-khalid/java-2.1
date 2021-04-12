bc235de7057570ce123791964b6b81db38b98d96
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-04-12 15:34:46 +0500

    Task 2 completed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // ********************Question 1**********************************
-var a = 1, b = 2, c = 3 ;
+var a = 1, b = 2, c = 3;
 
-// ********************Question 1**********************************
+// ********************Question 2 **********************************
 
+// document.write("<h1>IlLegal Variables</h1><ol><li>-a</li><li>my-room</li<li>My Room</li><li>1 My Room</li> <li>my Alert</li></ol><h1>Legal Variables</h1><ol><li>camelCase</li><li>my_room<li>my_room_1</li><li>myroom</li> <li>discriptive_variables</li></ol>"
+// )
