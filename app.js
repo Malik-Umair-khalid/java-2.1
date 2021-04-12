ec50a57e9904ea368ecfb3cae34a51ab3cf088f9
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-04-12 15:46:23 +0500

    Task 3 completed

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ var a = 1, b = 2, c = 3;
 
 // document.write("<h1>IlLegal Variables</h1><ol><li>-a</li><li>my-room</li<li>My Room</li><li>1 My Room</li> <li>my Alert</li></ol><h1>Legal Variables</h1><ol><li>camelCase</li><li>my_room<li>my_room_1</li><li>myroom</li> <li>discriptive_variables</li></ol>"
 // )
+
+// ********************Question 3 **********************************
+
+// document.write("<h1>Rules for naming JS variables</h1> \n <p>Variable names can only contain String, Number, Legal and illegal.")
+// document.write("<p>Variables must begin with a Letter, $ or Name. For example $name, _name or name</p>")
+// document.write("Variable names are case Sensitive")
+// document.write("<p>Variable names should not be JS Keywords</p>")
